396ec6c820034dd70759be1b4337db311138eb8e
erics118 <52634785+erics118@users.noreply.github.com>
2026-03-17 21:51:33 -0400

    simplify

--- src/aaa.tsx
+++ src/aaa.tsx
@@ -4,22 +4,30 @@ import { useState } from "react";
 export default function Command() {
   const [showDetail, setShowDetail] = useState(true);
   return (
-    <List
-      navigationTitle="a"
-      isShowingDetail={showDetail}
-      actions={
-        <ActionPanel>
-          <Action
-            title="Toggle Show Detail"
-            onAction={() => {
-              setShowDetail(!showDetail);
-            }}
-          />
-        </ActionPanel>
-      }
-    >
-      {Array.from({ length: 20 }, (_, i) => 5 * (i + 1)).map((num) => (
-        <List.Item key={num} title={String(num).repeat(10)} subtitle={"a".repeat(num)} />
+    <List navigationTitle="a" isShowingDetail={showDetail}>
+      {[
+        ["5----------", "aaaaa"],
+        ["10---------", "aaaaaaaaaa"],
+        ["15---------", "aaaaaaaaaaaaaaa"],
+        ["20---------", "aaaaaaaaaaaaaaaaaaaa"],
+        ["25---------", "aaaaaaaaaaaaaaaaaaaaaaaaa"],
+        ["30---------", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"],
+        ["35---------", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"],
+        ["40---------", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"],
+      ].map(([title, subtitle]) => (
+        <List.Item
+          key={title}
+          title={title}
+          subtitle={subtitle}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Toggle Show Detail"
+                onAction={() => setShowDetail(!showDetail)}
+              />
+            </ActionPanel>
+          }
+        />
       ))}
     </List>
   );
